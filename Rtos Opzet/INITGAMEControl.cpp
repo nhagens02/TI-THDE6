832bb1f45280365b479548ecdefdc0845f209b1b
pim12345 <60601873+pim12345@users.noreply.github.com>
2020-10-26 13:33:08 +0100

    update rtos opzet

--- Rtos Opzet/INITGAMEControl.cpp
+++ Rtos Opzet/INITGAMEControl.cpp
@@ -1,8 +1,24 @@
 #include "hwlib.hpp"
 #include "rtos.hpp"
 
-enum eButtonID = {instellingenKnopID, minKnopID, plusKnopID, okKnopID};
+enum eButtonID = {BUTTON_1,BUTTON_2,BUTTON_3,BUTTON_4,BUTTON_5,BUTTON_6,BUTTON_7,BUTTON_8,BUTTON_9,BUTTON_0,BUTTON_*,BUTTON_#,BUTTON_A,BUTTON_B,BUTTON_C,BUTTON_D};
 
 class INITGAMEControl : public rtos::task<>{
+	enum state_t = {idle,init,enterPlayTime,setGameMode,SetTimeUntilStart,sendData};
+
+	private:
+		state_t state = idle;
+		registerGameParametersControl& registerGameParametersControl;
+		dataToIRByteControl& dataToIRByteControl;
+
+	INITGAMEControl(registerGameParametersControl& registerGameParametersControl,dataToIRByteControl& dataToIRByteControl):
+
+	{buttonHandler.addButton();}
+
+
+	public:
+		void buttonPressed(eButtonID buttonID){buttonChannel.write(buttonID);}
+
+	private:
 
 };
--- Rtos Opzet/ReceiveIRMessageControl.cpp
+++ Rtos Opzet/ReceiveIRMessageControl.cpp
@@ -1,8 +1,6 @@
 #include "hwlib.hpp"
 #include "rtos.hpp"
 
-enum eButtonID = {instellingenKnopID, minKnopID, plusKnopID, okKnopID};
-
 class ReceiveIRMessageControl : public rtos::task<>{
 
 };
