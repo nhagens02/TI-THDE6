9ad232c8fd7d43ba89009bf84afa2e3e4a4b8f04
marhotiewer <marhotiewer@protonmail.com>
2020-10-28 13:31:27 +0100

    Use 38Khz carrier frequency

--- mark/ir_send/main.cpp
+++ mark/ir_send/main.cpp
@@ -1,11 +1,13 @@
 #include "hwlib.hpp"
 
+#define BITS sizeof(uint16_t) * 8
+
 // Ouput the 38KHz carrier frequency for the required time in microseconds
 // This is timing critial and just do-able on an Arduino using the standard I/O functions.
 // If you are using interrupts, ensure they disabled for the duration.
 void IRcarrier(unsigned int IRtimemicroseconds)
 {
-	auto ir = hwlib::target::d2_36kHz();
+	auto ir = hwlib::target::pin_out(hwlib::target::pins::d2);
 	
 	for(unsigned int i=0; i < (IRtimemicroseconds / 26); i++)
 	{
@@ -23,53 +25,14 @@ void IRcarrier(unsigned int IRtimemicroseconds)
 	}
 }
 
-void testIR() {
-	auto ir = hwlib::target::d2_36kHz();
-	
-	ir.write(true);
-	ir.flush();
-	
-	hwlib::wait_us(50);
-	
-	ir.write(false);
-	ir.flush();
-	
-	hwlib::wait_us(50);
-	
-	ir.write(true);
-	ir.flush();
-	
-	hwlib::wait_us(50);
-	
-	ir.write(false);
-	ir.flush();
-	
-}
-
 void sendIRMessage(uint16_t code) {
-	auto ir = hwlib::target::d2_36kHz();
-	
 	for(uint8_t i = 0; i < 16; i++) {
 		if(code & (1 << (BITS - 1))) {
-			ir.write(true);
-			ir.flush();
-			
-			hwlib::wait_us(1600);
-			
-			ir.write(false);
-			ir.flush();
-			
+			IRcarrier(1600);
 			hwlib::wait_us(800);
 		}
 		else {
-			ir.write(true);
-			ir.flush();
-			
-			hwlib::wait_us(800);
-			
-			ir.write(false);
-			ir.flush();
-			
+			IRcarrier(800);
 			hwlib::wait_us(1600);
 		}
 		code <<= 1;
@@ -77,20 +40,20 @@ void sendIRMessage(uint16_t code) {
 }
 
 void detectButtonPress() {
-	auto sw = hwlib::target::pin_in(hwlib::target::pins::d43);
-	
+	auto sw_ = hwlib::target::pin_in(hwlib::target::pins::d43);
+	auto sw = hwlib::invert(sw_);
+	bool lastBtnState = false;
+
 	while(true) {
-		static bool lastBtnState = false;
-		
 		sw.refresh();
 		bool state = sw.read();
 		
 		if (state != lastBtnState) {
 			lastBtnState = state;
-			if (state) {
-				uint16_t code = 0b1110101010101010;
+			if (!state) {
+				uint16_t code = 0b1010101010101010;
 				sendIRMessage(code);
-				hwlib::cout << "Button pressed!\n";
+				hwlib::cout << "IR Packet sent!\n";
 			}
 		}
 	}
