c38158eeeeb523b9d85f06ea1bec2d39b527916d
marhotiewer <marhotiewer@protonmail.com>
2020-10-29 10:18:28 +0100

    Moved ir_led to project folder so Arduino IDE stops complaining

--- mark/ir_recieve/main.cpp
+++ mark/ir_recieve/main.cpp
@@ -1,25 +1,25 @@
 #include "hwlib.hpp"
 
-int main(void){
+int main(void) {
    
    namespace target = hwlib::target;
    
    auto tsop_signal = target::pin_in(target::pins::d8);
    auto tsop_gnd    = target::pin_out(target::pins::d9);
    auto tsop_vdd    = target::pin_out(target::pins::d10);
+   auto led         = target::pin_out(target::pins::led);
+   
    tsop_gnd.write(0);
    tsop_vdd.write(1);
    tsop_gnd.flush();
    tsop_vdd.flush();
-   
-   auto led         = target::pin_out(target::pins::led);
    
    auto const active = 100'000;
    auto last_signal = hwlib::now_us() - active;
    
    for(;;){
       tsop_signal.refresh();
-      if(tsop_signal.read() == 0){
+      if(tsop_signal.read() == 0) {
          last_signal = hwlib::now_us();
       }
       led.write((last_signal + active) > hwlib::now_us());
