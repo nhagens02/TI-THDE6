8222b20e99c5128d21ae77f019782f2396acd578
pim12345 <60601873+pim12345@users.noreply.github.com>
2020-10-30 14:55:14 +0100

    Update SoundControl.cpp

--- Rtos Opzet/SoundControl.cpp
+++ Rtos Opzet/SoundControl.cpp
@@ -1,8 +1,50 @@
 #include "hwlib.hpp"
 #include "rtos.hpp"
 
-enum eButtonID = {instellingenKnopID, minKnopID, plusKnopID, okKnopID};
 
 class SoundControl : public rtos::task<>{
+	enum state_t state = {idle,playSound};
 
+	private:
+		state_t = idle;
+		rtos::pool playSoundIDPool;
+		rtos::flag playSoundFlag;
+
+
+	SoundControl():
+
+	{}
+
+	public:
+		void playSound(int soundID){playSoundID.write(soundID);playSoundFlag.set();}
+
+	private:
+		void main(){
+
+			for(;;){
+				switch(state)
+				{
+					case idle:
+						//entry events
+
+						//other events
+						hwlib::wait(playSoundFlag);
+						int soundID = playSoundIDPool.read();
+						state = playSound;
+						break;
+
+					case playSound:
+						//entry events
+						auto sound = soundFiles.getSoundFiles(soundID);
+						playSound(sound);
+						
+						//other events
+						state = idle;
+						break;
+
+					default:break;
+
+				}	
+			}
+		}
 };
