df34554caeef03f9404dba17f0498567ec5785e7
pim12345 <60601873+pim12345@users.noreply.github.com>
2020-11-02 13:58:22 +0100

    update rtos opzet

---
+++ Rtos Opzet/IRLed.cpp
@@ -0,0 +1,23 @@
+#include "hwlib.hpp"
+#include "rtos.hpp"
+
+class IRLed {
+private:
+	hwlib::pin_out& ledPin;
+
+public:
+	IRLed(hwlib::pin_out& ledPin):
+		ledPin(ledPin)
+    {}
+
+    void turnOn(){
+		ledPin.write(1);
+		ledPin.flush();
+    }
+    void turnOff(){
+		ledPin.write(0);
+		ledPin.flush();
+    }
+
+};
+
