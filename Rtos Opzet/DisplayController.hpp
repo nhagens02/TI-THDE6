79ebd6dd2fe2c730165fc77d7a8451a4fbaeb6ec
Ronnie-Stuivenberg <59872536+Ronnie-Stuivenberg@users.noreply.github.com>
2020-11-04 16:39:08 +0100

    Update DisplayController.hpp
    Doxygen aanpassingen

--- Rtos Opzet/DisplayController.hpp
+++ Rtos Opzet/DisplayController.hpp
@@ -9,7 +9,9 @@
 /// DisplayController CLASS
 /// \details
 /// This class will show the right information on the screen. 
-/// This class uses rtos::task<>. 
+/// This class uses rtos::task<>, hwlib::pin_oc, hwlib::i2c_bus_bit_banged_scl_sda i2c_bus,
+/// hwlib::glcd_oled, hwlib::window_part_t, hwlib::font_default_16x16, hwlib::terminal_from, 
+/// hwlib::font_default_8x8, hwlib::terminal_from, rtos::pool, rtos::flag, hwlib::wait_ms. 
 class DisplayController : public rtos::task<> {
 	enum state_t {idle, };
 	private:
